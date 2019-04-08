f01efa9f026cd66b963b45a3b5655d5d121e759a
Cadu <Cadu@CADU-PC>
2019-04-07 22:21:54 -0300

    Criação do Contexto.

---
+++ InfnetTecCsharpCrud/InfnetTecCsharpCrud/Infraestrutura/Context.cs
@@ -0,0 +1,25 @@
+﻿using InfnetTecCsharpCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace InfnetTecCsharpCrud.Infraestrutura
+{
+    public class Context : DbContext
+    {
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<PessoaFisica> PessoaFisica { get; set; }
+        public DbSet<PessoaJuridica> PessoaJuridica { get; set; }
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
